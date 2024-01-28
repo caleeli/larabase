5ed390c8b6acf0be5d08bff533d61db28696e95e
David Callizaya <davidcallizaya@gmail.com>
2024-01-28 09:27:46 -0400

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { createVuePlugin as vue } from 'vite-plugin-vue2';
+import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig(({ command, mode }) => {
 
